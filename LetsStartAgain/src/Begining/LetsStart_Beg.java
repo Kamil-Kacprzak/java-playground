bd46a20539f79af1fd472c8e9d1d4e1835945bdb
Kamil-Kacprzak <kamil.kacprzak@yahoo.com>
2019-12-09 21:53:37 +0100

    Initial commit

---
+++ LetsStartAgain/src/Begining/LetsStart_Beg.java
@@ -0,0 +1,17 @@
+/**
+ * Description: Following project is a quick jump into semantic of Java SE and git.
+ */
+package Begining;
+
+/**
+ * @author Bonobo
+ *
+ * Script date: Dec 9, 2019
+ */
+public class LetsStart_Beg {
+	public static void main(String[] args) {
+		System.out.println("Hello again ;]");
+				
+	}
+
+}
