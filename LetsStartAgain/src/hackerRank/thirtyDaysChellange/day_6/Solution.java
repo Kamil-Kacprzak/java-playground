693719b90ad1a076c90797f801ce96c0b6de21fa
Kamil-Kacprzak <kamil.kacprzak@yahoo.com>
2020-02-15 19:53:19 +0100

    Day 6  finished.

--- LetsStartAgain/src/hackerRank/thirtyDaysChellange/day_6/Solution.java
+++ LetsStartAgain/src/hackerRank/thirtyDaysChellange/day_6/Solution.java
@@ -6,40 +6,43 @@ import java.util.*;
 public class Solution {
 
 	private static final Scanner scan = new Scanner(System.in);
+	private static final int EVEN = 0;
+	private static final int ODD  = 1;
 	
-	private static String evenOddStrings(String s) {
-		String even = new String();
-		String odd	= new String();
-		for (int i = 0; i<s.toCharArray().length;i++) {
-			if ( i%2 == 0) {
-				char[] tmp = s.toCharArray();
-				even.concat(String.valueOf(tmp.toString().charAt(i)));
-			}else {
-				char[] tmp = s.toCharArray();
-				odd.concat(String.valueOf(tmp.toString().charAt(i)));
-			}
-			return even.concat(String.copyValueOf("\r\n"+odd.chars()))
-		}
+	private static String[] evenOddStringSeparator(String s) {
+		String[] results = new String[] {"",""};
+		char 	 charAt;
 		
+		for (int i = 0; i<s.toCharArray().length;i++) {
+			
+			if ( i%2 == 0) {
+				charAt = s.charAt(i);
+				results[EVEN] = results[EVEN].concat(String.valueOf(charAt));
+			}else {
+				charAt = s.charAt(i);
+				results[ODD] = results[ODD].concat(String.valueOf(charAt));
+			}	
+		}
+		return results;
+	}
 	
 	public static void main(String[] args) {
-        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
-		BufferWriter bw  
 		
 		int n = scan.nextInt();
-	     scan.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
-	     evenOddStrings
-		String s = scan.nextLine();
-        scan.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
-        
-        
-        
-       
-        
-        
-        	
-        
-        
+	    scan.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+	    String[] input = new String[n];
+	    
+	    for( int i = 0; i<n; i++) {
+		   input[i] = scan.nextLine();
+	    }
+	    
+	    String[] output;
+	    
+	    for (String s : input) {
+	    	output = evenOddStringSeparator(s);
+	    	System.out.println(output[EVEN]+" "+output[ODD]);
+	    }
+
         scan.close();
     }
 }
